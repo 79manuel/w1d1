0e87251c798704a334d2519916136e64a8e53dab
Manuel <prezcabellomanuel@gmail.com>
2017-09-05 05:15:26 +0000

    Refactored password.js file

--- password.js
+++ password.js
@@ -1,20 +1,11 @@
 var args = process.argv.slice(2);
 
 function obfuscate(password){
-  var string = "";
-  for (i = 0; i < password.length; i++) {
-    if(password[i] === "a"){
-      string += "4";
-    } else if (password[i] === "e") {
-      string += "3"
-    } else if (password[i] === "o") {
-      string += "0"
-    } else if (password[i] === "l") {
-      string += "1";
-    } else{
-      string += password[i];
-    }
-  }
+  var string = password.replace(/a/g, "4");
+  string = string.replace(/e/g, "3");
+  string = string.replace(/o/g, "0");
+  string = string.replace(/l/g, "1");
+
   return string;
 }
 
